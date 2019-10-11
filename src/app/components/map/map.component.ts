4c3fb25804d9d8f101356e7dd014e3f7bc8001a9
Alexandre J <alexandre.journet@gmail.com>
2019-10-11 17:06:43 +0200

    Modif map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AgmCoreModule } from '@agm/core';
+import { AgmCoreModule, MarkerManager, AgmMarker } from '@agm/core';
+import { Marker } from '@agm/core/services/google-maps-types';
 
 @Component({
   selector: 'app-map',
@@ -12,9 +13,41 @@ export class MapComponent implements OnInit {
   longitude = 4.850000;
   mapType = 'roadmap';
 
-  constructor() { }
+  parentMessage = '';
+
+  selectedMarker;
+  markers;
+
+  constructor() {  }
 
   ngOnInit() {
+    this.initMap();
+  }
+
+  initMap(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.markers = [{lat: position.coords.latitude, lng: position.coords.longitude, alpha: 1}];
+      });
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  }
+
+  addMarker(lat: number, lng: number) {
+    this.markers.push({ lat, lng});
+  }
+
+  selectMarker(event: { latitude: string; longitude: string; }) {
+    this.parentMessage = '';
+    this.parentMessage += event.latitude + '/';
+    this.parentMessage += event.longitude;
+  }
+
+  placeMarker(event) {
+    this.addMarker(event.coords.lat, event.coords.lng);
   }
 
 }
